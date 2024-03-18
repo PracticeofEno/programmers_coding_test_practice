0e1ea4604ccd9116ead8c8a5938eccba73c67acd
Eno <k1990121@gmail.com>
2024-03-18 19:06:59 +0900

    list 정렬 방식에서 우선순위큐를 사용하게 변경하였으나 여전히 시간초과 한번의 반복문에 O(1)로 접근하는 형태로 바꾸어볼것

--- overtime_index.py
+++ overtime_index.py
@@ -4,14 +4,26 @@
 # n만큼 works에서 수치를 뺼 수 있음. 즉 큰 수부터 n만큼 빼주면 됨
 # 정렬된 자료구조를 가지며, 가장 큰 수부터 가져와서 1씩 빼주는걸 반복하면 됨
 # 여기서 효율적으로 만들고자 할수 있겠지만 가장 간단한 알고리즘으로써 list를 만들고 빼주고 정렬하고 형태로 선택
+from queue import PriorityQueue
+
+
 def solution(n, works: list):
-    works.sort(reverse=True)
-    for i in range(n):
-        if works[0] == 0:
+    pq = PriorityQueue()
+    for work in works:
+        print(f"get priority: {work}, work: {-work}")
+        pq.put((-work, work))
+    for _ in range(n):
+        if pq.empty():
+            break
+        priority, work = pq.get()
+        print(f"get priority: {priority}, work: {work}")
+        if work == 1:
             continue
-        works[0] -= 1
-        works.sort(reverse=True)
-    overtime_index = sum([i**2 for i in works])
+        print(f"put priority: {priority + 1}, work: {work - 1}")
+        print("")
+        pq.put((priority + 1, work - 1))
+    data = [work for _, work in pq.queue]
+    overtime_index = sum([i ** 2 for i in data])
     print(overtime_index)
     return overtime_index
 
